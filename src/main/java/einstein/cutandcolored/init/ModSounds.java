e45b8ed63bbc97d17809efb2e83254a957ae4d46
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2021-05-09 18:18:56 -0400

    #2 Fixed

--- src/main/java/einstein/cutandcolored/init/ModSounds.java
+++ src/main/java/einstein/cutandcolored/init/ModSounds.java
@@ -3,7 +3,10 @@ package einstein.cutandcolored.init;
 import einstein.cutandcolored.CutAndColored;
 import einstein.einsteins_library.util.RegistryHandler;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
+@EventBusSubscriber(modid = CutAndColored.MODID, bus = Bus.MOD)
 public class ModSounds
 {
     public static final SoundEvent UI_GLASSCUTTER_TAKE_RESULT = RegistryHandler.registerSound(CutAndColored.MODID, "ui.glasscutter_take_result");
