6c0a145a527654d6e9da50f20d0a4138ce53e125
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2022-05-23 15:11:52 -0400

    Wool stairs and slabs are mined faster with shears now

---
+++ src/main/java/einstein/cutandcolored/mixin/AddShearableBlocks.java
@@ -0,0 +1,30 @@
+package einstein.cutandcolored.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import einstein.cutandcolored.data.BlockTagsGenerator;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShearsItem;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+@Mixin(ShearsItem.class)
+public class AddShearableBlocks {
+	
+	@Inject(at = @At("RETURN"), method = "mineBlock(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/LivingEntity;)Z", cancellable = true)
+	private void mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity entity, CallbackInfoReturnable<Boolean> returnable) {
+		returnable.setReturnValue(!state.is(BlockTagsGenerator.WOOL_STAIRS) && !state.is(BlockTagsGenerator.WOOL_SLABS));
+	}
+
+	@Inject(at = @At("HEAD"), method = "getDestroySpeed(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/block/state/BlockState;)F", cancellable = true)
+	private void getDestroySpeed(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> returnable) {
+		if (state.is(BlockTagsGenerator.WOOL_STAIRS) || state.is(BlockTagsGenerator.WOOL_SLABS)) {
+			returnable.setReturnValue(5.0F);
+		}
+	}
+}
